1be2655797a3fb649a1dd9edd8a53a541865e9f9
Oliver <oliver.jelenic@fina.hr>
2020-09-01 13:12:40 +0200

    Updates

---
+++ precache-manifest.13211366643b7cff7d9af3546bb45d8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bdf40de7ebc9f7c053d70d68c6f0ab",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "0ba38fcf30357710352e",
+    "url": "/monsters-react/static/css/main.48663f45.chunk.css"
+  },
+  {
+    "revision": "5d25a3a634dd49baba5c",
+    "url": "/monsters-react/static/js/2.aea4af21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.aea4af21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba38fcf30357710352e",
+    "url": "/monsters-react/static/js/main.1e5ad9e2.chunk.js"
+  },
+  {
+    "revision": "313df3f9e6a46568ac4f",
+    "url": "/monsters-react/static/js/runtime-main.5561a303.js"
+  },
+  {
+    "revision": "e96f186fe61b4059889e293b3f5a02ed",
+    "url": "/monsters-react/static/media/orange-juice.e96f186f.ttf"
+  }
+]);
