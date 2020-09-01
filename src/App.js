0078af82c396073312886ba310f96ecc640b7187
Oliver <oliver.jelenic@fina.hr>
2020-09-01 13:17:27 +0200

    dodavanje fileova na git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import  {CardList} from "./components/card-list/card-list.component";
+import { SearchBox } from './components/search-box/search-box.component';
+import Title from './components/title/title';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({monsters: users}));
+  }
+
+  render(){
+    //Destructuring 
+    const{monsters, searchField} = this.state;
+    // to je isto kao
+    /*cost monsters = this.state.monsters;
+    cost searchField = this.state.searchField;
+    */
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title/>
+        <SearchBox 
+          placeholder='Search ...'
+          handleChange={e => this.setState({searchField: e.target.value})}
+          />
+      <CardList monsters = {filteredMonsters}/>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/title/title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './title-style.css'
+
+function Title() {
+    return (
+
+        <div className="robotsFont">
+            <span className="monstersFont">Monsters & </span>
+            <span>Robots</span>
+            <span className="monstersFont"> inc.</span>
+        </div>
+    )
+}
+
+export default Title
